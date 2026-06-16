2b4530a2e4e9eebe89d8b8080e1a03691565067a
sx <taylor.vieira@estudante.ifms.edu.br>
2026-06-15 21:42:44 -0300

    menu feito

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
-sync:const prompt = require(‘prompt-sync’)();
- 
+const prompt = require('prompt-sync')();
+const { ContaPoupanca, ContaCorrente } = require('./Contas.js')
+
+let contas = [];
+let opcao = "";
+
+
+function mostrarContas() {
+
+  if (contas.length === 0) {
+    console.log("Nenhuma conta criada.");
+    return false;
+  }
+
+  console.log("\n=== CONTAS ===");
+
+  for (let i = 0; i < contas.length; i++) {
+    console.log(`${i} - ${contas[i].titular}`);
+  }
+
+  return true;
+}
+
+while (opcao !== "0") {
+
+  console.log(`
+ SISTEMA BANCÁRIO 
+1. Criar Conta Poupança
+2. Criar Conta Corrente
+3. Depositar
+4. Sacar
+5. Transferir
+6. Ver Extrato
+7. Aplicar rendimento
+0. Sair
+`);}
+
+opcao = prompt("Escolha uma opção: ");
